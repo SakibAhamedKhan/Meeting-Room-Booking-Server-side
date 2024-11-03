1c2acc2a7ebecb8f8b83a6dba3bcc1d068b89938
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2024-11-03 22:14:15 +0600

    Cookies added on response in login

--- src/config/index.ts
+++ src/config/index.ts
@@ -3,15 +3,16 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export default {
-    port: process.env.PORT,
-    db_url: process.env.DB_URL,
-    bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
-    jwt_access_secret: process.env.JWT_ACCESS_SECRET,
-    jwt_access_expries: process.env.JWT_ACCESS_EXPRIES,
-    jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
-    jwt_refresh_expries: process.env.JWT_REFRESH_EXPRIES,
+  port: process.env.PORT,
+  db_url: process.env.DB_URL,
+  bcrypt_salt_rounds: process.env.BCRYPT_SALT_ROUNDS,
+  jwt_access_secret: process.env.JWT_ACCESS_SECRET,
+  jwt_access_expries: process.env.JWT_ACCESS_EXPRIES,
+  jwt_refresh_secret: process.env.JWT_REFRESH_SECRET,
+  jwt_refresh_expries: process.env.JWT_REFRESH_EXPRIES,
+  node_env: process.env.NODE_ENV,
 
-    admin_name: process.env.ADMIN_NAME,
-    admin_email: process.env.ADMIN_EMAIL,
-    admin_password: process.env.ADMIN_PASSWORD,
-}
+  admin_name: process.env.ADMIN_NAME,
+  admin_email: process.env.ADMIN_EMAIL,
+  admin_password: process.env.ADMIN_PASSWORD,
+};
--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -3,6 +3,7 @@ import { catchAsync } from "../../utils/catchAsync";
 import httpStatus from "http-status-codes";
 import { USER_ROLE } from "../user/user.constant";
 import { AuthService } from "./auth.service";
+import config from "../../config";
 
 const signup = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -23,13 +24,20 @@ const signup = catchAsync(
 const login = catchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
     const userData = req.body;
-    const result = await AuthService.login(userData);
+    const {accessToken, refreshToken} = await AuthService.login(userData);
 
+    res.cookie("refreshToken",refreshToken, {
+      httpOnly: true, 
+      secure: config.node_env==="production",
+    })
+    
     res.status(200).json({
       success: true,
       statusCode: 200,
       message: "Login successfully",
-      data: result,
+      data: {
+        accessToken,
+      },
     });
   }
 );
