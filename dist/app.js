c725727fb3be794b2b4c0729a396e9d0694636e8
unknown <sakibahamedkhan01@gmail.com>
2024-12-20 16:27:02 +0600

    updated 20 december

--- dist/app.js
+++ dist/app.js
@@ -17,7 +17,7 @@ const app = (0, express_1.default)();
 //parser
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
-app.use((0, cors_1.default)({ origin: ["http://localhost:5173"], credentials: true }));
+app.use((0, cors_1.default)({ origin: ["http://localhost:5173", "https://meeting-room-frontend.vercel.app"], credentials: true }));
 // all routes
 app.use("/api/auth", auth_route_1.AuthRoutes);
 app.use("/api/rooms", room_route_1.RoomRoutes);
--- src/app.ts
+++ src/app.ts
@@ -14,7 +14,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 
-app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+app.use(cors({ origin: ["http://localhost:5173", "https://meeting-room-frontend.vercel.app"], credentials: true }));
 
 // all routes
 app.use("/api/auth", AuthRoutes);
